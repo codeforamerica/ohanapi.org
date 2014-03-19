35643ed692bf53a69c6726ae6f6a19a7c80c2122
Anselm Bradford <git@anselmbradford.com>
2014-03-18 17:48:17 -0700

    Adds grunt dev environment
    Adds grunt, website source, and compass SASS config file.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ohanapi.org
 ===========
 
 The ohanapi.org website for the Ohana API project.
+
+## Installation
+
+  Use `npm install` to install the required node modules for grunt.
+
+  Use `grunt production` to compile a production deploy in the `www` directory.
+
+  Use `grunt gh-pages --gh-pages-message 'Making commits'` to add a custom commit message when pushing to `gh-pages` branch using grunt.
---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-gh-pages');
+
+	grunt.initConfig({
+		browserify: {
+      production: {
+        src: ['src/main.js'],
+        dest: 'www/js/main.js'
+      } // production
+    }, // browserify
+		compass: {
+			compile: {
+				options: {
+					config: 'compass-config.rb'
+				} //options
+			} //compile
+		}, //compass
+		clean: ["www"], // clean
+		copy: {
+		  main: {
+		    files: [
+		      // copy root files
+		      {expand: true, flatten: true, src: ['src/*'], dest: 'www/', filter: 'isFile'},
+
+		      // copy img directory
+		      {expand: true, cwd: 'src/', src: ['img/**'], dest: 'www/'},
+
+		      // copy css directory
+		      {expand: true, cwd: 'src/', src: ['css/*'], dest: 'www/'},
+
+		      // copy helper.js
+		      {expand: true, cwd: 'src/', src: ['js/helper.js'], dest: 'www/'},
+
+		    ] //files
+		  } //main
+		}, //copy
+		'gh-pages': {
+	    options: {
+	      base: 'www',
+	      clone: 'tmp',
+      	push: false
+	    },
+	    src: ['**/*']
+	  }, // gh-pages
+		watch: {
+			scripts: {
+				files: ['src/js/app/**/*.js'],
+			}, //scripts
+			sass: {
+				files: ['src/sass/*.scss'],
+				tasks: ['compass:dev']
+			}, //sass
+			html: {
+				files: ['*.html']
+			} //html
+		} //watch
+	}) //initConfig
+	grunt.registerTask('default', 'watch');
+	grunt.registerTask('production', ['browserify:production','compass:compile','clean','copy','gh-pages']);
+	grunt.registerTask('development', ['browserify:production','compass:compile','clean','copy']);
+} //exports
